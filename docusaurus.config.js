c8864e20be31c7da42c68c8f0944f8c8b45f7a8e
Ben Capper <BenCapper19@gmail.com>
2021-11-28 17:47:25 +0000

    Updates
    Signed-off-by: Ben Capper <BenCapper19@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -85,7 +85,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} Tutors. Built with Docusaurus.`,
       },
       prism: {
         theme: lightCodeTheme,
